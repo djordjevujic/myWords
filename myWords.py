b4c3a5439eaf0ceb31df70daff20009013daf0ba
Djordje Vujic <djordjevujic@yahoo.com>
2018-12-25 11:31:17 +0100

    new lessons

--- myWords.py
+++ myWords.py
@@ -1,9 +1,8 @@
 # @todo test with random selection
 # @todo test with language selecting
+# @todo don't remove \n from the last line in file
 
-file_name = "testWords.txt"
-
-test_file = open(file_name, 'r')
+file_name = "4lesson.txt"
 
 # print("1. Test kroz ceo fajl")
 print("Known words:")
@@ -19,10 +18,12 @@ if words_option < 1 or words_option > 2:
 points = 0
 max_points = 0
 
+test_file = open(file_name, 'r')
+
 for line in test_file:
     max_points += 1
 
-test_file = open(file_name, 'r')
+test_file.seek(0)
 
 i = 1
 
@@ -31,17 +32,20 @@ for line in test_file:
     words_lst = line.split("-")
 
     if words_option == 1:
-        known_words = words_lst[0].split(",")    # Get all known words into one list
-        expected_words = words_lst[1].split(",") # Get all expected words into one list
+        known_words = words_lst[0].split(",")       # Get all known words into one list
+        expected_words = words_lst[1].split(",")    # Get all expected words into one list
     else:
-        known_words = words_lst[1].split(",")  # Get all known words into one list
-        expected_words = words_lst[0].split(",")  # Get all expected words into one list
+        known_words = words_lst[1].split(",")       # Get all known words into one list
+        expected_words = words_lst[0].split(",")    # Get all expected words into one list
 
     answer = False
 
     print(str(i) +". {} - ".format(known_words))
 
     inputed_word = input()
+
+    if(inputed_word == "exit()"):
+        exit()
 
     for awaited_word in expected_words:
         if inputed_word == awaited_word:
